1a88e99a2147831adc9c24de9ba4fd68978fd1b5
Smogg21 <spazzaglia98@gmail.com>
2024-04-09 17:48:44 -0500

    Added filter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import {products as initialProducts} from './mocks/products.json'
+import { Products } from "./components/Products"
+import { useState } from 'react'
+
 function App () {
-  return <>Shopping Cart </>
+  const [products] = useState(initialProducts)
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0, 
+  })
+
+  const filterProducts = (products) =>{
+    return products.filter(product =>{
+      return (
+        product.price >= filters.minPrice &&
+        (
+          filters.category === 'all' ||
+          product.category === filters.category
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
+  return (
+    <Products products={filteredProducts}/>
+  )
 }
 
 export default App
